7155ba952fa4487243589c6991ef38761bdcd653
Leandro lacayo <leandrolacayo17@gmail.com>
2023-01-26 11:56:54 -0600

    Clase de hoy

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace jueves2623
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnadd = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.DuDCatalogo = new System.Windows.Forms.DomainUpDown();
+            this.SuspendLayout();
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(375, 95);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(75, 23);
+            this.btnadd.TabIndex = 0;
+            this.btnadd.Text = "Agregar";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.Location = new System.Drawing.Point(375, 160);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(75, 23);
+            this.btndelete.TabIndex = 1;
+            this.btndelete.Text = "Eliminar";
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Location = new System.Drawing.Point(142, 121);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(35, 13);
+            this.lblresultado.TabIndex = 2;
+            this.lblresultado.Text = "label1";
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(145, 39);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(170, 20);
+            this.txtnombre.TabIndex = 3;
+            // 
+            // DuDCatalogo
+            // 
+            this.DuDCatalogo.Location = new System.Drawing.Point(145, 210);
+            this.DuDCatalogo.Name = "DuDCatalogo";
+            this.DuDCatalogo.Size = new System.Drawing.Size(170, 20);
+            this.DuDCatalogo.TabIndex = 4;
+            this.DuDCatalogo.Text = "-Seleccione-";
+            this.DuDCatalogo.SelectedItemChanged += new System.EventHandler(this.DuDCatalogo_SelectedItemChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 359);
+            this.Controls.Add(this.DuDCatalogo);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnadd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Label lblresultado;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.DomainUpDown DuDCatalogo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jueves2623
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          /*  DuDCatalogo.Items.Add("Celulares");
+            DuDCatalogo.Items.Add("Computadoras");
+            DuDCatalogo.Items.Add("Impresoras");
+          */
+        }
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            DuDCatalogo.Items.Add(txtnombre.Text);
+            txtnombre.Text =  "";
+        }
+
+        private void DuDCatalogo_SelectedItemChanged(object sender, EventArgs e)
+        {
+            string producto = DuDCatalogo.SelectedItem.ToString();
+            lblresultado.Text = string.Format("Ha seleccionado: "+producto);
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            int eliminar_indice=DuDCatalogo.SelectedIndex;
+            DuDCatalogo.Items.RemoveAt(eliminar_indice);
+
+        }
+    }
+}
